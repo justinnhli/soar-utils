18175e0dea3f2ed192e670da40fcc406d94762b5
justinnhli <justinnhli@gmail.com>
2014-05-05 12:54:36 -0400

    pep8 cleanup

--- soar-utils.py
+++ soar-utils.py
@@ -12,180 +12,180 @@ import sys
 # dynamically find the Soar trunk
 module_path = [p + "/Python_sml_ClientInterface.py" for p in sys.path if exists(join(p, "Python_sml_ClientInterface.py")) and "trunk" in p][0]
 with open(module_path) as fd:
-	module = load_module("Python_sml_ClientInterface", fd, module_path, ('.py', 'U', 1))
+    module = load_module("Python_sml_ClientInterface", fd, module_path, ('.py', 'U', 1))
 import Python_sml_ClientInterface as sml
 from state2dot import state2dot
 
 # low level Soar functions
 
 def create_kernel():
-	kernel = sml.Kernel.CreateKernelInCurrentThread()
-	if not kernel or kernel.HadError():
-		print("Error creating kernel: " + kernel.GetLastErrorDescription())
-		exit(1)
-	return kernel
+    kernel = sml.Kernel.CreateKernelInCurrentThread()
+    if not kernel or kernel.HadError():
+        print("Error creating kernel: " + kernel.GetLastErrorDescription())
+        exit(1)
+    return kernel
 
 def create_agent(kernel, name):
-	agent = kernel.CreateAgent("agent")
-	if not agent:
-		print("Error creating agent: " + kernel.GetLastErrorDescription())
-		exit(1)
-	return agent
+    agent = kernel.CreateAgent("agent")
+    if not agent:
+        print("Error creating agent: " + kernel.GetLastErrorDescription())
+        exit(1)
+    return agent
 
 # mid-level framework
 
 def cli(agent):
-	agent.RegisterForPrintEvent(sml.smlEVENT_PRINT, callback_print_message, None)
-	command = raw_input("soar> ")
-	while command not in ("exit", "quit"):
-		if command:
-			cmd = command.strip().split()[0]
-			if cmd in COMMANDS:
-				print(COMMANDS[cmd](command))
-			else:
-				print(agent.ExecuteCommandLine(command).strip())
-		command = raw_input("soar> ")
+    agent.RegisterForPrintEvent(sml.smlEVENT_PRINT, callback_print_message, None)
+    command = raw_input("soar> ")
+    while command not in ("exit", "quit"):
+        if command:
+            cmd = command.strip().split()[0]
+            if cmd in COMMANDS:
+                print(COMMANDS[cmd](command))
+            else:
+                print(agent.ExecuteCommandLine(command).strip())
+        command = raw_input("soar> ")
 
 def parameterize_commands(param_map, commands):
-	return [cmd.format(**param_map) for cmd in commands]
+    return [cmd.format(**param_map) for cmd in commands]
 
 def run_parameterized_commands(agent, param_map, commands):
-	for cmd in parameterize_commands(param_map, commands):
-		result = agent.ExecuteCommandLine(cmd)
+    for cmd in parameterize_commands(param_map, commands):
+        result = agent.ExecuteCommandLine(cmd)
 
 def param_permutations(params):
-	keys = sorted(params.keys())
-	for values in product(*(params[key] for key in keys)):
-		yield dict(zip(keys, values))
+    keys = sorted(params.keys())
+    for values in product(*(params[key] for key in keys)):
+        yield dict(zip(keys, values))
 
 # IO
 
 def parse_output_commands(agent, structure):
-	commands = {}
-	mapping = {}
-	for cmd in range(0, agent.GetNumberCommands()):
-		error = False
-		command = agent.GetCommand(cmd)
-		cmd_name = command.GetCommandName()
-		if cmd_name in structure:
-			parameters = {}
-			for param_name in structure[cmd_name]:
-				param_value = command.GetParameterValue(param_name)
-				if param_value:
-					parameters[param_name] = param_value
-			if not error:
-				commands[cmd_name] = parameters
-				mapping[cmd_name] = command
-		else:
-			error = True
-		if error:
-			command.AddStatusError()
-	return commands, mapping
+    commands = {}
+    mapping = {}
+    for cmd in range(0, agent.GetNumberCommands()):
+        error = False
+        command = agent.GetCommand(cmd)
+        cmd_name = command.GetCommandName()
+        if cmd_name in structure:
+            parameters = {}
+            for param_name in structure[cmd_name]:
+                param_value = command.GetParameterValue(param_name)
+                if param_value:
+                    parameters[param_name] = param_value
+            if not error:
+                commands[cmd_name] = parameters
+                mapping[cmd_name] = command
+        else:
+            error = True
+        if error:
+            command.AddStatusError()
+    return commands, mapping
 
 def dot_to_input(edges):
-	pass
+    pass
 
 # callback registry
 
 def register_print_callback(kernel, agent, function, user_data=None):
-	agent.RegisterForPrintEvent(sml.smlEVENT_PRINT, function, user_data)
+    agent.RegisterForPrintEvent(sml.smlEVENT_PRINT, function, user_data)
 
 def register_output_callback(kernel, agent, function, user_data=None):
-	agent.RegisterForRunEvent(sml.smlEVENT_AFTER_OUTPUT_PHASE, function, user_data)
+    agent.RegisterForRunEvent(sml.smlEVENT_AFTER_OUTPUT_PHASE, function, user_data)
 
 def register_output_change_callback(kernel, agent, function, user_data=None):
-	kernel.RegisterForUpdateEvent(sml.smlEVENT_AFTER_ALL_GENERATED_OUTPUT, function, user_data)
+    kernel.RegisterForUpdateEvent(sml.smlEVENT_AFTER_ALL_GENERATED_OUTPUT, function, user_data)
 
 def register_destruction_callback(kernel, agent, function, user_data=None):
-	agent.RegisterForRunEvent(sml.smlEVENT_AFTER_HALTED, function, user_data)
+    agent.RegisterForRunEvent(sml.smlEVENT_AFTER_HALTED, function, user_data)
 
 # callback functions
 
 def callback_print_message(mid, user_data, agent, message):
-	print(message.strip())
+    print(message.strip())
 
 def print_report_row(mid, user_data, agent, *args):
-	condition = user_data["condition"]
-	param_map = user_data["param_map"]
-	domain = user_data["domain"]
-	reporters = user_data["reporters"]
-	if condition(param_map, domain, agent):
-		pairs = []
-		pairs.extend("=".join([k, str(v)]) for k, v in param_map.items())
-		pairs.extend("{}={}".format(*reporter(param_map, domain, agent)) for reporter in reporters)
-		print(" ".join(pairs))
+    condition = user_data["condition"]
+    param_map = user_data["param_map"]
+    domain = user_data["domain"]
+    reporters = user_data["reporters"]
+    if condition(param_map, domain, agent):
+        pairs = []
+        pairs.extend("=".join([k, str(v)]) for k, v in param_map.items())
+        pairs.extend("{}={}".format(*reporter(param_map, domain, agent)) for reporter in reporters)
+        print(" ".join(pairs))
 
 def report_data_wrapper(param_map, domain, reporters, condition=None):
-	if condition is None:
-		condition = (lambda param_map, domain, agent: True)
-	return {
-			"condition": condition,
-			"param_map": param_map,
-			"domain": domain,
-			"reporters": reporters,
-			}
+    if condition is None:
+        condition = (lambda param_map, domain, agent: True)
+    return {
+        "condition": condition,
+        "param_map": param_map,
+        "domain": domain,
+        "reporters": reporters,
+    }
 
 # common reporters
 
 def soar_path(param_map, domain, agent):
-	return ("computed_branch", inspect.getfile(sml))
+    return ("computed_branch", inspect.getfile(sml))
 
 def num_decisions(param_map, domain, agent):
-	result = re.sub("^([^ ]*) decisions.*", r"\1", agent.ExecuteCommandLine("stats"), flags=re.DOTALL)
-	return ("num_decisions", result)
+    result = re.sub("^([^ ]*) decisions.*", r"\1", agent.ExecuteCommandLine("stats"), flags=re.DOTALL)
+    return ("num_decisions", result)
 
 def avg_decision_time(param_map, domain, agent):
-	result = re.sub(".*\((.*) msec/decision.*", r"\1", agent.ExecuteCommandLine("stats"), flags=re.DOTALL)
-	return ("avg_dc_msec", result)
+    result = re.sub(".*\((.*) msec/decision.*", r"\1", agent.ExecuteCommandLine("stats"), flags=re.DOTALL)
+    return ("avg_dc_msec", result)
 
 def max_decision_time(param_map, domain, agent):
-	result = re.sub(".*  Time \(sec\) *([0-9.]*).*", r"\1", agent.ExecuteCommandLine("stats -M"), flags=re.DOTALL)
-	return ("max_dc_msec", float(result) * 1000)
+    result = re.sub(".*  Time \(sec\) *([0-9.]*).*", r"\1", agent.ExecuteCommandLine("stats -M"), flags=re.DOTALL)
+    return ("max_dc_msec", float(result) * 1000)
 
 def kernel_cpu_time(param_map, domain, agent):
-	result = re.sub(".*Kernel CPU Time: *([0-9.]*).*", r"\1", agent.ExecuteCommandLine("stats"), flags=re.DOTALL)
-	return ("kernel_cpu_msec", float(result) * 1000)
+    result = re.sub(".*Kernel CPU Time: *([0-9.]*).*", r"\1", agent.ExecuteCommandLine("stats"), flags=re.DOTALL)
+    return ("kernel_cpu_msec", float(result) * 1000)
 
 # new commands
 
 def command_viz(command):
-	command = command.strip()
-	path = re.search("(--path|-p)\s+([^ ]+)", command)
-	if path:
-		command = re.sub("\s+(--path|-p)\s+([^ ]+)", "", command)
-	if command == "viz":
-		args = "--depth 1000 <s>"
-	else:
-		args = command[4:]
-	dot = state2dot(agent.ExecuteCommandLine("print " + args))
-	if path:
-		path = path.group(2)
-		with open(path, "w") as fd:
-			fd.write(dot)
-			fsync(fd)
-			return "state graph writted to {}".format(path)
-		return "an unknown error occured trying to write to {}".format(path)
-	return dot
+    command = command.strip()
+    path = re.search("(--path|-p)\s+([^ ]+)", command)
+    if path:
+        command = re.sub("\s+(--path|-p)\s+([^ ]+)", "", command)
+    if command == "viz":
+        args = "--depth 1000 <s>"
+    else:
+        args = command[4:]
+    dot = state2dot(agent.ExecuteCommandLine("print " + args))
+    if path:
+        path = path.group(2)
+        with open(path, "w") as fd:
+            fd.write(dot)
+            fsync(fd)
+            return "state graph writted to {}".format(path)
+        return "an unknown error occured trying to write to {}".format(path)
+    return dot
 
 COMMANDS = {
-		"viz":command_viz,
-		}
+    "viz": command_viz,
+}
 
 # soar code management
 
 def make_branch(branch):
-	try:
-		stdout = check_output(("make-branch", branch), stderr=STDOUT)
-		return True
-	except CalledProcessError as cpe:
-		return False
+    try:
+        stdout = check_output(("make-branch", branch), stderr=STDOUT)
+        return True
+    except CalledProcessError as cpe:
+        return False
 
 if __name__ == "__main__":
-	kernel = create_kernel()
-	agent = create_agent(kernel, "agent")
-	for source in sys.argv[1:]:
-		print(agent.ExecuteCommandLine("source " + source))
-	cli(agent)
-	kernel.DestroyAgent(agent)
-	kernel.Shutdown()
-	del kernel
+    kernel = create_kernel()
+    agent = create_agent(kernel, "agent")
+    for source in sys.argv[1:]:
+        print(agent.ExecuteCommandLine("source " + source))
+    cli(agent)
+    kernel.DestroyAgent(agent)
+    kernel.Shutdown()
+    del kernel
--- state2dot.py
+++ state2dot.py
@@ -3,60 +3,60 @@
 import re
 
 def state2dot(state):
-	state = "\n".join(line.strip() for line in state.split("\n"))
-	state = re.sub(r'\n\^', " ^", state)
-	while re.search("\([^ ]+ \^[^ ]+ (\|[^|]*\||[^| )]+)( \+)? \^", state):
-		state = re.sub("\(([^ ]+) (\^[^ ]+ (\|[^|]*\||[^| )]+)( \+)?) \^", r"(\1 \2)\n(\1 ^", state)
-	# at this point, everything is in (<id> ^attr val) triples... if it's not -i/--internal
-	lines = set()
-	count = 0
-	for line in state.split("\n"):
-		line = line.strip()
-		if not line:
-			continue
-		# if the input is from -i/--internal, need to strip a few things
-		# we can recognized this by checking for an internal ID
-		if re.match("\([0-9]+:", line):
-			# first, extract other stuff
-			iid = re.sub("^\(([0-9]+):.*", r'\1', line)
-			flags = re.sub(".*(\[[0-9.]+\])(( \+)?)(( :[0-9A-Za-z-]*)*)\)$", r'\1\4', line).split()
-			wma = flags[0][1:-1]
-			flags = flags[1:]
-			# then return the line to as if it wasn't -i/--internal
-			line = re.sub("^\(([0-9]+): ", "(", line)
-			line = re.sub(" \[[0-9.]*\](( \+)?).*?$", r'\1)', line)
-		else:
-			iid = ""
-			flags= []
-			wma = ""
-		# all that's left is the actual (<id> ^attr val)
-		ident, attr, value, accept = re.match("^\(([^ ]+) \^([^ ]+) (.*?)(( \+)?)\)$", line).groups()[0:4]
-		if value.startswith("@"):
-			lines.add('"' + value + '" [shape="doublecircle"]')
-		elif not re.match("^[A-Z][0-9]+$", value):
-			lines.add('"temp' + str(count) + '" [label="' + value + '", shape="box"]')
-			count += 1
-			value = "temp{}".format(count - 1)
-		if wma:
-			lines.add('"{}" -> "{}" [label="{}{}\\n[{}]"]'.format(ident, value, attr, accept, wma))
-		else:
-			lines.add('"{}" -> "{}" [label="{}{}"]'.format(ident, value, attr, accept))
-	result = ["digraph {"]
-	result.append('	node [shape="circle"];')
-	result.append("	" + ";\n	".join(lines) + ";")
-	result.append("}")
-	result = "\n".join(result)
-	return result
+    state = "\n".join(line.strip() for line in state.split("\n"))
+    state = re.sub(r'\n\^', " ^", state)
+    while re.search("\([^ ]+ \^[^ ]+ (\|[^|]*\||[^| )]+)( \+)? \^", state):
+        state = re.sub("\(([^ ]+) (\^[^ ]+ (\|[^|]*\||[^| )]+)( \+)?) \^", r"(\1 \2)\n(\1 ^", state)
+    # at this point, everything is in (<id> ^attr val) triples... if it's not -i/--internal
+    lines = set()
+    count = 0
+    for line in state.split("\n"):
+        line = line.strip()
+        if not line:
+            continue
+        # if the input is from -i/--internal, need to strip a few things
+        # we can recognized this by checking for an internal ID
+        if re.match("\([0-9]+:", line):
+            # first, extract other stuff
+            iid = re.sub("^\(([0-9]+):.*", r'\1', line)
+            flags = re.sub(".*(\[[0-9.]+\])(( \+)?)(( :[0-9A-Za-z-]*)*)\)$", r'\1\4', line).split()
+            wma = flags[0][1:-1]
+            flags = flags[1:]
+            # then return the line to as if it wasn't -i/--internal
+            line = re.sub("^\(([0-9]+): ", "(", line)
+            line = re.sub(" \[[0-9.]*\](( \+)?).*?$", r'\1)', line)
+        else:
+            iid = ""
+            flags = []
+            wma = ""
+        # all that's left is the actual (<id> ^attr val)
+        ident, attr, value, accept = re.match("^\(([^ ]+) \^([^ ]+) (.*?)(( \+)?)\)$", line).groups()[0:4]
+        if value.startswith("@"):
+            lines.add('"' + value + '" [shape="doublecircle"]')
+        elif not re.match("^[A-Z][0-9]+$", value):
+            lines.add('"temp' + str(count) + '" [label="' + value + '", shape="box"]')
+            count += 1
+            value = "temp{}".format(count - 1)
+        if wma:
+            lines.add('"{}" -> "{}" [label="{}{}\\n[{}]"]'.format(ident, value, attr, accept, wma))
+        else:
+            lines.add('"{}" -> "{}" [label="{}{}"]'.format(ident, value, attr, accept))
+    result = ["digraph {"]
+    result.append('    node [shape="circle"];')
+    result.append("    " + ";\n    ".join(lines) + ";")
+    result.append("}")
+    result = "\n".join(result)
+    return result
 
 if __name__ == "__main__":
-	import sys
-	text = ""
-	if len(sys.argv) > 1:
-		result = []
-		for file in sys.argv[1:]:
-			with open(file, "r") as fd:
-				result.append(fd.read())
-		text = "".join(result)
-	elif not sys.stdin.isatty():
-		text = sys.stdin.read()
-	print(state2dot(text))
+    import sys
+    text = ""
+    if len(sys.argv) > 1:
+        result = []
+        for file in sys.argv[1:]:
+            with open(file, "r") as fd:
+                result.append(fd.read())
+        text = "".join(result)
+    elif not sys.stdin.isatty():
+        text = sys.stdin.read()
+    print(state2dot(text))
